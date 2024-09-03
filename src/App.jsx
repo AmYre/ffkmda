fbe94849f43ba3e3c5c9c6e31ed88d76e4869759
AmYre <benhammouda.amir@gmail.com>
2024-09-03 20:20:42 +0200

    working poc

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,23 @@
 import { useState, useEffect } from "react";
-import { GoogleMap, LoadScript, Marker, InfoWindow, useJsApiLoader } from "@react-google-maps/api";
+import Map, { Marker, Popup } from "react-map-gl";
+import MapGL from "./MapGL";
 import axios from "axios";
 import logo from "/logo.png";
 import "./App.css";
 
 const App = () => {
 	const [clubs, setClubs] = useState([]);
-	const [isApiLoaded, setIsApiLoaded] = useState(false);
-
-	const { isLoaded } = useJsApiLoader({
-		id: "google-map-script",
-		googleMapsApiKey: "AIzaSyCGFP9MhDHistZ8GX9rLndTI-wSYCfsxzU",
-	});
 
 	useEffect(() => {
-		if (!window.google) {
-			const script = document.createElement("script");
-			script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCGFP9MhDHistZ8GX9rLndTI-wSYCfsxzU&callback=initMap`;
-			script.async = true;
-			script.defer = true;
-			window.initMap = () => setIsApiLoaded(true);
-			document.head.appendChild(script);
-		} else {
-			setIsApiLoaded(true);
-		}
-
-		return () => {
-			if (window.google) {
-				delete window.google;
-			}
-		};
+		axios
+			.get(`https://sports-sgsocialgouv.opendatasoft.com/api/records/1.0/search/?dataset=passsports-asso_volontaires&q=kickboxing&rows=1000`)
+			.then((response) => {
+				setClubs(response.data.records);
+			})
+			.catch((error) => {
+				console.error("Il y a eu une erreur!", error);
+			});
 	}, []);
-
-	useEffect(() => {
-		if (location) {
-			axios
-				.get(`https://sports-sgsocialgouv.opendatasoft.com/api/records/1.0/search/?dataset=passsports-asso_volontaires&q=kickboxing&rows=1000`)
-				.then((response) => {
-					setClubs(response.data.records);
-				})
-				.catch((error) => {
-					console.error("Il y a eu une erreur!", error);
-				});
-		}
-	}, [location]);
 
 	return (
 		<>
@@ -52,26 +26,8 @@ const App = () => {
 					<img src={logo} className="logo" alt="Vite logo" />
 				</a>
 			</div>
-			<h3>Trouver un club de Kickboxing</h3>
-			<LoadScript googleMapsApiKey="AIzaSyCGFP9MhDHistZ8GX9rLndTI-wSYCfsxzUY">
-				<GoogleMap mapContainerStyle={{ width: "100%", height: "400px" }} center={{ lat: 49.30853, lng: 0.967377 }} zoom={10}>
-					{/* {clubs &&
-					clubs.map(
-						(club, index) =>
-							club.fields.geoloc_finale && (
-								<Marker key={index} position={{ lat: club.fields.geoloc_finale[0], lng: club.fields.geoloc_finale[1] }}>
-									<InfoWindow>
-										<div>
-											<h2>{club.fields.nom}</h2>
-											<p>{club.fields.adresse}</p>
-											<p>{club.fields.commune}</p>
-										</div>
-									</InfoWindow>
-								</Marker>
-							)
-					)} */}
-				</GoogleMap>
-			</LoadScript>
+			<h3>Trouver un club de Kickboxing autour de chez vous</h3>
+			<MapGL clubs={clubs} />
 		</>
 	);
 };
---
+++ src/MapGL.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZGV2b2x1dGlvbiIsImEiOiJjbTBtb3R0a2wwNTVlMmxzZHF4ejE0ODV5In0.KY6NT4AUHqFrhjpIDVwOrw";
+
+const MapGL = ({ clubs }) => {
+	const mapContainerRef = useRef(null);
+	const [location, setLocation] = useState({ latitude: 2.3, longitude: 48.8 });
+
+	useEffect(() => {
+		const map = new mapboxgl.Map({
+			container: mapContainerRef.current,
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: [location.longitude, location.latitude],
+			zoom: 10,
+		});
+
+		clubs.forEach((club) => {
+			if (club.fields.geoloc_finale) {
+				new mapboxgl.Marker()
+					.setLngLat([club.fields.geoloc_finale[1], club.fields.geoloc_finale[0]])
+					.setPopup(
+						new mapboxgl.Popup({ offset: 25 }).setHTML(
+							`<h2>${club.fields.nom}</h2><p>${club.fields.adresse}</p><p>${club.fields.com_code} ${club.fields.commune}</p><p>${
+								club?.fields?.telephone ? club?.fields?.telephone : ""
+							} ${club?.fields?.courriel ? club?.fields?.courriel : ""}</p>`
+						)
+					)
+					.addTo(map);
+			}
+		});
+
+		return () => map.remove();
+	}, [clubs]);
+
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					setLocation({
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude,
+					});
+				},
+				(error) => {
+					setError(error.message);
+				}
+			);
+		} else {
+			setError("Geolocation is not supported by this browser.");
+		}
+	}, []);
+
+	clubs && console.log("clubs", clubs);
+	return <div ref={mapContainerRef} style={{ height: "400px", width: "800px" }} />;
+};
+
+export default MapGL;
